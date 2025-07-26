41db55ba5be0f1e66240d5d3e4f9807346504112
tak-tack <kth302303@gmail.com>
2025-07-26 23:29:29 +0900

    - 테코 시즌2 - 정렬(이중 우선순위큐 정답)

--- src/CodingTest/Sort/이중우선순위큐.java
+++ src/CodingTest/Sort/이중우선순위큐.java
@@ -18,69 +18,119 @@ public class 이중우선순위큐 {
     public static void main(String[] args) {
         String[] array =
                 //{"I 16","I -5643","D -1","D 1","D 1","I 123","D -1"};
-         {"I -45", "I 653", "D 1", "I -642", "I 45", "I 97", "D 1", "D -1", "I 333"};
+                //{"I -45", "I 653", "D 1", "I -642", "I 45", "I 97", "D 1", "D -1", "I 333"};
+        {"I -45", "I 653", "D 1", "I -642", "I 45", "I 97", "I 333"};
+                //"I -45",  "I -45", "D 1", "I 45", "I 97", "I 333"};
         이중우선순위큐 sol = new 이중우선순위큐();
-        int[] arr =  sol.solution(array);
-        for(int i=0;i<arr.length;i++){
+        int[] arr = sol.solution(array);
+        for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
     }
 
-    public int[] solution(String[] operations) {
-        int[] answer = {};
-        int small = 0;
-        int big = 0;
-        boolean reverseCheck = false; // false 일경우 작은값이 우선순위
+    // 내가 작성한 코드
+//    public int[] solution(String[] operations) {
+//        int[] answer = {};
+//        int small = 0;
+//        int big = 0;
+//        boolean reverseCheck = false; // false 일경우 작은값이 우선순위
+//
+//        //PriorityQueue<Integer> pq = new PriorityQueue<>();
+//        for (int i = 0; i < operations.length; i++) {
+//            if (operations[i].charAt(0) == 'I') {
+//                pq.offer(Integer.parseInt(operations[i].substring(2)));
+//            } else if (operations[i].charAt(0) == 'D' && operations[i].charAt(2) == '-' && !pq.isEmpty()) { // 최솟값 삭제
+//                if (reverseCheck) {
+//                    reverse();
+//                } else {
+//                    pq.poll();
+//                }
+//            } else if (operations[i].charAt(0) == 'D' && operations[i].charAt(2) != '-' && !pq.isEmpty()) { // 최댓값 삭제
+//                if (!reverseCheck) {
+//                    reverse();
+//                } else {
+//                    pq.poll();
+//                }
+//                reverseCheck = true;
+//            } else if (pq.isEmpty()) {
+//                System.out.println("ERROR~");
+//            }
+//        }
+//        if (!pq.isEmpty() && pq.size() > 1) {
+//            if (!reverseCheck) {
+//                small = pq.poll();
+//                //reverse();
+//                big = reverseAndPoll(reverseCheck);
+//            } else {
+//                big = pq.poll();
+//                //reverse();
+//                small = reverseAndPoll(reverseCheck);
+//            }
+//        }
+//        answer = new int[]{small, big};
+//        return answer;
+//    }
+//
+//    public static void reverse() {
+//        PriorityQueue<Integer> pqTemp = new PriorityQueue<>(Collections.reverseOrder());
+//        for (int i = 0; i < pq.size(); i++) {
+//            pqTemp.add(pq.poll());
+//        }
+//        pq = pqTemp;
+//    }
+//
+//    public static int reverseAndPoll(boolean reverseCheck) {
+//        while (pq.size() != 1) {
+//            if (!reverseCheck) { // 우선순위가 높은 숫자우선 정렬상태 -> small 을 구해야함
+//                int num1 = pq.poll();
+//                int num2 = pq.peek();
+//                if (num1 <= num2) {
+//                    pq.poll();
+//                    pq.add(num1);
+//                }
+//            } else {
+//                pq.poll();
+//            }
+//        }
+//        return pq.poll();
+//    }
+    // 정답지
+    public int[] solution(String[] arguments) {
+        int[] answer = {0,0};
 
-        //PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for(int i = 0; i < operations.length; i++){
-                if(operations[i].charAt(0) == 'I'){
-                    pq.offer(Integer.parseInt(operations[i].substring(2)));
-                }else if(operations[i].charAt(0) == 'D' && operations[i].charAt(2) == '-' && !pq.isEmpty()) { // 최솟값 삭제
-                    if(reverseCheck){
-                        reverse();
-                    }else{
-                        pq.poll();
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+        PriorityQueue<Integer> reverse_pq = new PriorityQueue<Integer>(Collections.reverseOrder());
+
+        for(int i=0; i<arguments.length; i++) {
+            String temp[] = arguments[i].split(" ");
+            switch(temp[0]) {
+                case "I" :
+                    pq.add(Integer.parseInt(temp[1]));
+                    reverse_pq.add(Integer.parseInt(temp[1]));
+                    break;
+                case "D" :
+                    if(pq.size() > 0) {
+                        if(Integer.parseInt(temp[1]) == 1) {
+                            // 최댓값 삭제
+                            int max = reverse_pq.poll();
+                            pq.remove(max);
+                        } else {
+                            // 최솟값 삭제
+                            int min = pq.poll();
+                            reverse_pq.remove(min);
+                        }
                     }
-                }else if(operations[i].charAt(0) == 'D' && operations[i].charAt(2) != '-' && !pq.isEmpty()){ // 최댓값 삭제
-                    if(!reverseCheck){
-                        reverse();
-                    }else{
-                        pq.poll();
-                    }
-                    reverseCheck = true;
-                }else {
-                    System.out.println("ERROR~");
-                }
-        }
-        if(!pq.isEmpty()){
-            if(!reverseCheck){
-                small= pq.poll();
-                //reverse();
-                big = reverseAndPoll();
-            }else {
-                big = pq.poll();
-                //reverse();
-                small = reverseAndPoll();
+                    break;
             }
         }
-        answer = new int[]{small, big};
-        return answer;
-    }
-    public static void reverse() {
-        PriorityQueue<Integer> pqTemp = new PriorityQueue<>(Collections.reverseOrder());
-        for(int i=0; i < pq.size(); i++){
-            pqTemp.add(pq.poll());
+
+        if(pq.size() >= 2) {
+            answer[0] = reverse_pq.poll();
+            answer[1] = pq.poll();
         }
-        pq = pqTemp;
-        //System.out.println(pq);
-    }
-    public static int reverseAndPoll(){
-        PriorityQueue<Integer> pqTemp = new PriorityQueue<>(Collections.reverseOrder());
-//        for(int i=0; i < pq.size(); i++){
-//            pq.poll();
-//        }
-        pqTemp = pq;
-        return pqTemp.poll();
+
+        System.out.println(answer[0] + ":" + answer[1]);
+
+        return answer;
     }
 }
